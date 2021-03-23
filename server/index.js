3032de3d13629439099d8ff4a650935501d069e7
DeividasKaraliunas <deikingas@gmail.com>
2021-03-23 09:49:33 +0200

    feat: added middlewares

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config({path:'server/.env'})
 
 const app = express();
+
+app.use(cors());
+
+const {SERVER_PORT} = process.env;
 
 app.get('/', (req,res) => {
     res.send('Testing...')
 })
 
-app.listen(5000);
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on http://localhost:${SERVER_PORT}`)
+});
