eea2da4092b238c8f72a711ec6e084515ecec33e
DeividasKaraliunas <deikingas@gmail.com>
2021-03-23 11:21:31 +0200

    feat: connected to Mongo Compass

--- server/index.js
+++ server/index.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config({path:'server/.env'})
+const morgan = require('morgan');
+const mongoose = require('mongoose')
+require('dotenv').config({ path: 'server/.env' })
 const animalRouter = require('./routes/animals')
 
 const server = express();
 
 server.use(cors());
+server.use(express.json())
+server.use(morgan('tiny'))
 
 server.use('/animals', animalRouter);
 
-const {SERVER_PORT} = process.env;
+const {
+    SERVER_PORT,
+    SERVER_USER,
+    SERVER_PASS,
+    SERVER_CLUSTER,
+    SERVER_NAME,
+} = process.env;
 
-server.get('/', (req,res) => {
+server.get('/', (req, res) => {
     res.send('Testing...')
 })
 
-server.listen(SERVER_PORT, () => {
-    console.log(`Server is running on http://localhost:${SERVER_PORT}`)
+mongoose.connect(`mongodb+srv://${SERVER_USER}:${SERVER_PASS}@${SERVER_CLUSTER}/${SERVER_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true })
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('Server is running')
+    server.listen(SERVER_PORT, () => {
+        console.log(`Server is running on http://localhost:${SERVER_PORT}`)
+    });
 });
+db.on('error', console.error.bind(console, 'connection error:'));
